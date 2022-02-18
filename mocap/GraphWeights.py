c3f30451f024508ba9e38ae679c20bfa0fee481d
Hardik Goel <hgoel7@gatech.edu>
2022-02-18 12:35:44 -0500

    Implemented a script to calculate weights

--- mocap/GraphWeights.py
+++ mocap/GraphWeights.py
@@ -1,37 +1,9 @@
 import queue
+import random
+import numpy as np
 
-def bfs(graph, source, dist):
-    Q = queue.Queue()
-    visited_vertices = set()
-    Q.put(source)
-    distance = 0
-    dist[source] = 0
-    visited_vertices.update({0})
-    while not Q.empty():
-        vertex = Q.get()
-        distance += 1
-        print(vertex, end="-->")
-        for u in graph[vertex]:
-            if u not in visited_vertices:
-                Q.put(u)
-                dist[u] = distance
-                visited_vertices.update({u})
-
-def distanceWeights(num):
-    # returns dictionary of weights to use for each arm
-    graph = {
-        1: [2, 4, 5],
-        2: [1, 3, 4, 5, 6],
-        3: [2, 5, 6],
-        4: [1, 2, 5, 7, 8],
-        5: [1, 2, 3, 4, 6, 7, 8, 9],
-        6: [2, 3, 5, 8, 9],
-        7: [4, 5, 8],
-        8: [4, 5, 6, 7, 9],
-        9: [5, 6, 8]
-    }
-
-    dist = {
+def bfs(graph, start):
+    discoveryMap = {
         1: -1,
         2: -1,
         3: -1,
@@ -43,10 +15,54 @@ def distanceWeights(num):
         9: -1
     }
 
-    bfs(graph, num, dist)
+    Q = queue.Queue()
+    visited = set()
 
-    print()
-    print(dist)
+    Q.put(start)
+    visited.add(start)
 
-distanceWeights(1)
+    while not Q.empty():
+        v = Q.get()
+        for u in graph[v]:
+            if u not in visited:
+                Q.put(u)
+                discoveryMap[u] = v
+                visited.add(u)
 
+    return discoveryMap
+
+def distanceWeights(arm):
+    weights = {}
+
+    neighbor_graph = {
+        1: [2, 4, 5],
+        2: [1, 3, 4, 5, 6],
+        3: [2, 5, 6],
+        4: [1, 2, 5, 7, 8],
+        5: [1, 2, 3, 4, 6, 7, 8, 9],
+        6: [2, 3, 5, 8, 9],
+        7: [4, 5, 8],
+        8: [4, 5, 6, 7, 9],
+        9: [5, 6, 8]
+    }
+
+    discovery = bfs(neighbor_graph, arm)
+
+    for i in discovery.keys():
+        distance = -1
+        j = i
+        while j != -1:
+            j = discovery.get(j)
+            distance += 1
+
+        weight = np.interp(distance, [0, 2], [1, 0.1])
+
+        w1 = int(100*(weight - 0.2))
+        w2 = int(100*(weight + 0.2))
+
+        weights[i] = abs(random.randint(w1, w2)/100)
+
+    return weights
+
+for x in range(0, 9):
+    print(x)
