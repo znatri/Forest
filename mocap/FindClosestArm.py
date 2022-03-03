ed356d12a9f2971c1d51a673171a19f08950cc6f
Hardik Goel <hgoel7@gatech.edu>
2022-03-03 14:18:25 -0500

    Script to find closest arm

---
+++ mocap/FindClosestArm.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def closest_arm(pos, nodes):
+    # nodes = np.asarray(nodes)
+    deltas = nodes - pos
+    dist_2 = np.einsum('ij,ij->i', deltas, deltas)
+    return np.argmin(dist_2)
+
+if __name__ == "__main__":
+    graph = np.array([[0.0, 0.0], [1.0, 0.0], [2.0, 0.0], [0.0, 1.0], [1.0, 1.0], [2.0, 1.0], [0.0, 2.0], [1.0, 2.0], [2.0, 2.0]])
+    pos = [2.1, 1.2]
+    print(closest_arm(pos, graph))
