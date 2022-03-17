af50636887f0ec0d860d52ca5293042fe5946acb
Hardik Goel <hgoel7@gatech.edu>
2022-03-16 20:42:23 -0400

    bugs need to be fixed

--- livetraj/LiveTraj.py
+++ livetraj/LiveTraj.py
@@ -17,73 +17,71 @@ def setup():
 
 
 def playRobot(que, arm):
-    q_i = [0 for i in range(0, 7)]
-    q_dot_i = [0 for i in range(0, 7)]
-    q_dot_f = [0 for i in range(0, 7)]
-    q_dotdot_i = [0 for i in range(0, 7)]
-    q_dotdot_f = [0 for i in range(0, 7)]
-
+    tf = 50
     t_step = 0.006
-    tf = 3
     t_array = np.arange(0, tf, t_step)
 
+    q_dot_f = np.zeros(7)
+    q_dotdot_f = np.zeros(7)
     p = [0, 0, 0, 90, 0, 0, 0]
 
-    goal = que.get()
-    q_f = goal
+    while True:
+        q = que.get()
 
-    j = 0
+        goal = q
 
-    while j <= len(t_array):
-        start_time = time.time()
+        q_i = p
+        q_dot_i = np.zeros(7)
+        q_dotdot_i = np.zeros(7)
+        q_f = goal
 
-        cont = False
-        for m, n in zip(p, q_f):
-            if (abs(m - n) > 1.0):
-                cont = True
+        j = 0
 
-        if cont:
-            break
+        while j < len(t_array):
+            start_time = time.time()
 
-        if j == len(t_array):
-            t = tf
-        else:
-            t = t_array[j]
+            if abs(p[0] - q_f[0]) < 1.0 and abs(p[1] - q_f[1]) < 1.0 and abs(p[2] - q_f[2]) < 1.0 and abs(
+                    p[3] - q_f[3]) < 1.0 and abs(p[4] - q_f[4]) < 1.0 and abs(p[5] - q_f[5]) < 1.0 and abs(
+                p[6] - q_f[6]) < 1.0:
+                break
 
-        a0 = q_i
-        a1 = q_dot_i
-        a2 = []
-        a3 = []
-        a4 = []
-        a5 = []
+            if j == len(t_array):
+                t = tf
+            else:
+                t = t_array[j]
 
-        for i in range(0, 7):
-            a2.append(0.5 * q_dotdot_i[i])
-            a3.append(1.0 / (2.0 * tf ** 3.0) * (
+            a0 = q_i
+            a1 = q_dot_i
+            a2 = []
+            a3 = []
+            a4 = []
+            a5 = []
+
+            for i in range(0, 7):
+                a2.append(0.5 * q_dotdot_i[i])
+                a3.append(1.0 / (2.0 * tf ** 3.0) * (
                         20.0 * (q_f[i] - q_i[i]) - (8.0 * q_dot_f[i] + 12.0 * q_dot_i[i]) * tf - (
-                            3.0 * q_dotdot_f[i] - q_dotdot_i[i]) * tf ** 2.0))
-            a4.append(1.0 / (2.0 * tf ** 4.0) * (
+                        3.0 * q_dotdot_f[i] - q_dotdot_i[i]) * tf ** 2.0))
+                a4.append(1.0 / (2.0 * tf ** 4.0) * (
                         30.0 * (q_i[i] - q_f[i]) + (14.0 * q_dot_f[i] + 16.0 * q_dot_i[i]) * tf + (
-                            3.0 * q_dotdot_f[i] - 2.0 * q_dotdot_i[i]) * tf ** 2.0))
-            a5.append(1.0 / (2.0 * tf ** 5.0) * (
+                        3.0 * q_dotdot_f[i] - 2.0 * q_dotdot_i[i]) * tf ** 2.0))
+                a5.append(1.0 / (2.0 * tf ** 5.0) * (
                         12.0 * (q_f[i] - q_i[i]) - (6.0 * q_dot_f[i] + 6.0 * q_dot_i[i]) * tf - (
-                            q_dotdot_f[i] - q_dotdot_i[i]) * tf ** 2.0))
+                        q_dotdot_f[i] - q_dotdot_i[i]) * tf ** 2.0))
 
-            p[i] = (a0[i] + a1[i] * t + a2[i] * t ** 2 + a3[i] * t ** 3 + a4[i] * t ** 4 + a5[i] * t ** 5)
+                p[i] = (a0[i] + a1[i] * t + a2[i] * t ** 2 + a3[i] * t ** 3 + a4[i] * t ** 4 + a5[i] * t ** 5)
 
-        arm.set_servo_angle_j(angles=p, is_radian=False)
-        print(f"{j} {p} {arm}")
+            arm.set_servo_angle_j(angles=p, is_radian=False)
+            # print(f"{p} {arm}")
 
-        tts = time.time() - start_time
-        sleep = 0.006 - tts
+            tts = time.time() - start_time
+            sleep = t_step - tts
 
-        if tts > 0.006:
-            sleep = 0
+            if tts > t_step:
+                sleep = 0
 
-        time.sleep(sleep)
-        j += 1
-
-    print(f"\nFinished {arm}")
+            time.sleep(sleep)
+            j += 1
 
 
 # Press the green button in the gutter to run the script.
@@ -112,88 +110,3 @@ if __name__ == '__main__':
     t2 = Thread(target=changeDir)
     t1.start()
     t2.start()
-
-
-
-    # IP = [0, 0, 0, 90, 0, 0, 0]
-    # tf = 3
-    #
-    # # t0 = 0
-    # t = [0 for i in range(0, 7)]
-    # q_i = [0 for i in range(0, 7)]
-    # q_dot_i = [0 for i in range(0, 7)]
-    # q_dot_f = [0 for i in range(0, 7)]
-    # q_dotdot_i = [0 for i in range(0, 7)]
-    # q_dotdot_f = [0 for i in range(0, 7)]
-    # t_array = np.arange(0, tf, 0.006)
-    # p = [0, 0, 0, 90, 0, 0, 0]
-    # v = [0 for i in range(0, 7)]
-    # a = [0 for i in range(0, 7)]
-    #
-    # # j_angles = [0, 0, 0, 90, 0, 0, 0]
-    #
-    # while True:
-    #     q = que.get()
-    #
-    #     # print("\nDequeued")
-    #     goal = q
-    #     q_i = p
-    #     # q_i = j_angles
-    #     q_dot_i = [0, 0, 0, 0, 0, 0, 0]
-    #     q_dotdot_i = [0, 0, 0, 0, 0, 0, 0]
-    #     q_f = goal
-    #     j = 0
-    #
-    #     while j <= len(t_array):
-    #         start_time = time.time()
-    #
-    #         if que.empty() == False:
-    #             goal = q.get()
-    #             q_i = p
-    #             q_dot_i = v
-    #             q_dotdot_i = [0 for i in range(0, 7)]
-    #             q_f = goal
-    #             j = 0
-    #             # IF YOU WANT TO ADD SPEED CHANGES THEN SWAP THE ABOVE LINES WITH THE BELOW LINES
-    #             # # q should input an array of [*absolute* position of joint, time(in seconds) to reach there]
-    #             # q_f = goal[0]
-    #             # tf = goal[1]
-    #             # t_array = np.arange(0, tf, 0.006)
-    #             print("switch")
-    #         if j == len(t_array):
-    #             t = tf
-    #         else:
-    #             t = t_array[j]
-    #
-    #         a0 = q_i
-    #         a1 = q_dot_i
-    #         a2 = []
-    #         a3 = []
-    #         a4 = []
-    #         a5 = []
-    #
-    #         for i in range(0, 7):
-    #             a2.append(0.5 * q_dotdot_i[i])
-    #             a3.append(1.0 / (2.0 * tf ** 3.0) * (20.0 * (q_f[i] - q_i[i]) - (8.0 * q_dot_f[i] + 12.0 * q_dot_i[i]) * tf - (3.0 * q_dotdot_f[i] - q_dotdot_i[i]) * tf ** 2.0))
-    #             a4.append(1.0 / (2.0 * tf ** 4.0) * (30.0 * (q_i[i] - q_f[i]) + (14.0 * q_dot_f[i] + 16.0 * q_dot_i[i]) * tf + (3.0 * q_dotdot_f[i] - 2.0 * q_dotdot_i[i]) * tf ** 2.0))
-    #             a5.append(1.0 / (2.0 * tf ** 5.0) * (12.0 * (q_f[i] - q_i[i]) - (6.0 * q_dot_f[i] + 6.0 * q_dot_i[i]) * tf - (q_dotdot_f[i] - q_dotdot_i[i]) * tf ** 2.0))
-    #
-    #             p[i] = (a0[i] + a1[i] * t + a2[i] * t ** 2 + a3[i] * t ** 3 + a4[i] * t ** 4 + a5[i] * t ** 5)
-    #             v[i] = (a1[i] + 2 * a2[i] * t + 3 * a3[i] * t ** 2 + 4 * a4[i] * t ** 3 + 5 * a5[i] * t ** 4)
-    #             a[i] = (2 * a2[i] + 6 * a3[i] * t + 12 * a4[i] * t ** 2 + 20 * a5[i] * t ** 3)
-    #
-    #         # j_angles = p
-    #         arm6.set_servo_angle_j(angles=p, is_radian=False)
-    #
-    #         tts = time.time() - start_time
-    #         sleep = 0.006 - tts
-    #
-    #         if tts > 0.006:
-    #             sleep = 0
-    #
-    #         time.sleep(sleep)
-    #         j += 1
-    #         # if t == 1:
-    #         # print(t, p, v, a)
-    #
-    #     # print("\nFinished")
